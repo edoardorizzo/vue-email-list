4bc1a4d126c160f53fee01078124dd1cf7a119a2
edoardorizzo <edo96.rizzo@gmail.com>
2023-04-11 16:03:23 +0200

    print one mail to screen

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,16 @@
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue',
+        mail: null
       }
+    },
+    mounted(){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( response => {
+            console.log(response);
+            this.mail = response.data.response
+        })
     }
   }).mount('#app')
